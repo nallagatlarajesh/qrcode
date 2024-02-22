babf84c32b98f1b249f0c41136b9da88117be424
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2024-02-22 17:52:50 +0530

    Create calender.py

---
+++ calender.py
@@ -0,0 +1,14 @@
+import calendar as cl
+yy=2011
+m=9
+print(cl.month(yy,m))
+
+import calendar as cl
+yyy=2034
+m=12
+print(cl.month(yyy,m))
+
+import calendar as cl
+abc=2050
+bn=5
+print(cl.month(abc,bn))
